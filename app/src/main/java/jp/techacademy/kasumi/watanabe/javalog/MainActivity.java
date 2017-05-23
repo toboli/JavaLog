7068fe7fcf01d13259a32b0be6a182cdffef1dbc
渡部かすみ <kasumisheep@gmail.com>
2017-05-23 22:42:18 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/kasumi/watanabe/javalog/MainActivity.java
@@ -0,0 +1,26 @@
+package jp.techacademy.kasumi.watanabe.javalog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int num1 = 10 + 5 - 2 * 4 / 2;
+        Log.d("javatest", "10 + 5 - 2 * 4 / 2 = " + String.valueOf(num1));
+
+        boolean flag1 = true;
+        boolean flag2 = false;
+        Log.d("javatest", "flag1 && flag2 = " + String.valueOf(flag1 && flag2));
+        Log.d("javatest", "flag1 || flag2 = " + String.valueOf(flag1 || flag2));
+
+        int num2 = 10;
+        int num3 = 20;
+        Log.d("javatest", "num2 < num3 = " + String.valueOf(num2 < num3));
+    }
+}
